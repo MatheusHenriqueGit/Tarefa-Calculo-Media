be9fbde130e6a9aa4182028cf60e0d91832f9802
MatheusHenriqueGit <MatheusContatoCr3@outlook.com>
2024-04-26 22:18:24 -0300

    calculo media

---
+++ src/Calculomedia.java
@@ -0,0 +1,16 @@
+
+public class Calculomedia {
+
+	public static void main(String[] args) {
+		int nota1 = 30;
+		int nota2 = 230;
+		int nota3 = 56;
+		int nota4 = 134;	
+		int divi = nota1 + nota2 + nota3 + nota4;
+		
+		System.out.println("divisão: " + divi / 4 );
+		
+
+	}
+
+}
